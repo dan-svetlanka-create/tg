d3c893ddb3a19f6c2446c0f16e5f37e1e1afdc0b
alexeys6969 <alexshashin.2019@gmail.com>
2025-12-17 13:18:22 +0500

    Созданный классы для приложения. Дан

---
+++ Classes/Events.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TelegramBot_Dan.Classes
+{
+    public class Events
+    {
+        /// <summary> Время в которое необходимо уведомить пользователя
+        public DateTime Time { get; set; }
+
+        /// <summary> Сообщение которое будет отправлено пользователем
+        public string Message { get; set; }
+
+        /// <summary> Конструктор для класса
+        public Events(DateTime time, string message)
+        {
+            Time = time; // запоминаем время когда необходимо уведомить пользователя
+            Message = message; // запоминаем сообщение
+        }
+    }
+}
---
+++ Classes/Users.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+
+namespace TelegramBot_Dan.Classes
+{
+    public class Users
+    {
+        /// <summary> Код пользователя
+        public long IdUser { get; set; }
+
+        /// <summary> События пользователя
+        public List<Events> Events { get; set; }
+
+        /// <summary> Конструктор для класса
+        public Users(long idUser)
+        {
+            IdUser = idUser; // запоминаем Id чата пользователя
+            Events = new List<Events>(); // инициализируем список событий
+        }
+    }
+}
+}
