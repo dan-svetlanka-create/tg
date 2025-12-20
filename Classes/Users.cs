c09b86ae4a95159011ebf11573a60ebf3f69fbf2
Sveta <dan-svetlanka@mail.ru>
2025-12-20 18:39:56 +0500

    Разработанный методы создания кнопок, и проверка на вводимую дату и время Дан

--- Classes/Users.cs
+++ Classes/Users.cs
@@ -23,4 +23,4 @@ namespace TelegramBot_Dan.Classes
         }
     }
 }
-}
+
--- Worker.cs
+++ Worker.cs
@@ -1,4 +1,8 @@
 using Telegram.Bot;
+using Telegram.Bot.Types.ReplyMarkups;
+using Telegram.Bot.Polling;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
 using TelegramBot_Dan.Classes;
 
 namespace TelegramBot_Dan
@@ -7,11 +11,11 @@ namespace TelegramBot_Dan
     {
 
         /// <summary> Токен телеграмм бота
-        readonly string token = "полученный телеграмм токен";
+        readonly string Token = "8335328272:AAFDyJHuPn_EMZ4ngQcrgYORj0esLU-NIy8";
 
         /// <summary> Клиент через который будет происходить взаимодействие с TelegramBotClient
         TelegramBotClient TelegramBotClient;
-
+        //private readonly IServiceScopeFactory _scopeFactory;
         /// <summary> Список пользователей
         List<Users> Users = new List<Users>();
 
@@ -60,5 +64,56 @@ namespace TelegramBot_Dan
                 await Task.Delay(1000, stoppingToken);
             }
         }
+        /// <summary>
+        /// Проверка корректности ввода даты и времени
+        /// </summary>
+        /// <param name="value">Значение которое будет преобразовано в дату</param>
+        /// <param name="time">Значение в которое будет записана скорректированная дата и время</param>
+        /// <returns>Результат преобразования значения в дату и время</returns>
+        public bool CheckFormatDateTime(string value, out DateTime time)
+        {
+            return DateTime.TryParse(value, out time);
+        }
+
+
+        /// <summary>
+        /// Создание кнопки для удаления всех задач
+        /// </summary>
+        /// <returns>Список кнопки</returns>
+        private static ReplyKeyboardMarkup GetButtons()
+        {
+            // Создаём список кнопок
+            List<KeyboardButton> keyboardButtons = new List<KeyboardButton>();
+            // Добавляем в список одну кнопку
+            keyboardButtons.Add(new KeyboardButton("Удалить все задачи"));
+            // Возвращаем список в списке
+            return new ReplyKeyboardMarkup
+            {
+                Keyboard = new List<List<KeyboardButton>>
+                {
+                    keyboardButtons
+                }
+            };
+        }
+
+
+        /// <summary>
+        /// Создание кнопки для удаления конкретного события
+        /// </summary>
+        /// <param name="Message">Сообщение</param>
+        /// <returns>Список кнопок</returns>
+        public static InlineKeyboardMarkup DeleteEvent(string Message)
+        {
+            // Создаём список кнопок
+            List<InlineKeyboardButton> inlineKeyboards = new List<InlineKeyboardButton>();
+            // Добавляем кнопку удалить, прикрепляя к ней текст, который необходимо удалить
+            inlineKeyboards.Add(new InlineKeyboardButton("Удалить", Message));
+            // Возвращаем список кнопок
+            return new InlineKeyboardMarkup(inlineKeyboards);
+        }
+
+
     }
 }
+
+
